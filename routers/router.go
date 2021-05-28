ec815edd261436eb164534a89391ce77aa9e7e7a
Grey <profile_1@163.com>
2021-05-28 17:38:25 +0800

    register router

--- routers/router.go
+++ routers/router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"github.com/Grey-12/gin-blog/pkg/setting"
+	v1 "github.com/Grey-12/gin-blog/routers/api/v1"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,10 +13,12 @@ func InitRouter() *gin.Engine {
 
 	gin.SetMode(setting.RunMode)
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Test",
-		})
-	})
+	apiV1 := r.Group("/api/v1")
+	{
+		apiV1.GET("/tags", v1.GetTags)
+		apiV1.POST("/tags", v1.AddTag)
+		apiV1.PUT("/tags/:id", v1.EditTag)
+		apiV1.DELETE("/tags/:id", v1.DeleteTag)
+	}
 	return r
 }
