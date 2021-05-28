06984cdbd2ec3ed8c720a75c28461b233b8b158a
Grey <profile_1@163.com>
2021-05-28 16:43:22 +0800

    init

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Grey-12/gin-blog/src/gin-blog/pkg/setting"
+
+)
+
+func main() {
+	fmt.Println(setting.PageSize)
+
+}
---
+++ pkg/util/pagination.go
@@ -0,0 +1,16 @@
+package util
+
+import (
+	"github.com/Grey-12/gin-blog/src/gin-blog/pkg/setting"
+	"github.com/Unknwon/com"
+	"github.com/gin-gonic/gin"
+)
+
+func GetPage(c *gin.Context) int {
+	result := 0
+	page, _ := com.StrTo(c.Query("page")).Int()
+	if page > 0 {
+		result = (page - 1) * setting.PageSize
+	}
+	return result
+}
