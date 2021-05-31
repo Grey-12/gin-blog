2a5eb6b28dfc14a1220a95895281a1ce7a82de31
Grey <profile_1@163.com>
2021-05-31 17:43:49 +0800

    GetArticles Add Article

--- routers/api/v1/article.go
+++ routers/api/v1/article.go
@@ -3,6 +3,8 @@ package v1
 import (
 	"github.com/Grey-12/gin-blog/models"
 	"github.com/Grey-12/gin-blog/pkg/errorCode"
+	"github.com/Grey-12/gin-blog/pkg/setting"
+	"github.com/Grey-12/gin-blog/pkg/util"
 	"github.com/astaxie/beego/validation"
 	"github.com/gin-gonic/gin"
 	"github.com/unknwon/com"
@@ -43,20 +45,89 @@ func GetArticle(c *gin.Context) {
 func GetArticles(c *gin.Context) {
 	data := make(map[string]interface{})
 	maps := make(map[string]interface{})
-	vaild := validation.Validation{}
+	valid := validation.Validation{}
 
 	var state int = -1
 	if arg := c.Query("state"); arg != "" {
 		state = com.StrTo(c.Query("state")).MustInt()
 		maps["state"] = state
 
-		vaild.Range(state, 0, 1, "state").Message("状态只允许0或1")
+		valid.Range(state, 0, 1, "state").Message("状态只允许0或1")
 	}
+
+	var tagId int = -1
+	if arg := c.Query("tag_id"); arg != "" {
+		tagId = com.StrTo(c.Query("tag_id")).MustInt()
+		maps["tag_id"] = tagId
+
+		valid.Min(tagId, 1, "tag_id").Message("标签ID必须大于0")
+	}
+
+	code := errorCode.INVALID_PARAMS
+	if !valid.HasErrors() {
+		code = errorCode.SUCCESS
+		data["lists"] = models.GetArticles(util.GetPage(c), setting.PageSize, maps)
+		data["total"] = models.GetArticleTotal(maps)
+
+	} else {
+		for _, err := range valid.Errors {
+			log.Println(err.Key, err.Message)
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg": errorCode.GetMsg(code),
+		"data": data,
+	})
+
 }
 
 // AddArticle 新增文章
 func AddArticle(c *gin.Context) {
+	tagId := com.StrTo(c.Query("tag_id")).MustInt()
+	title := c.Query("title")
+	desc := c.Query("desc")
+	content := c.Query("content")
+	createdBy := c.Query("created_by")
+	state := com.StrTo(c.DefaultQuery("state", "0")).MustInt()
 
+	valid := validation.Validation{}
+	valid.Min(tagId, 1, "tag_id").Message("标签ID必须大于0")
+	valid.Required(title, "title").Message("标题不能为空")
+	valid.Required(desc, "desc").Message("简述不能为空")
+	valid.Required(content, "content").Message("内容不能为空")
+	valid.Required(createdBy, "created_by").Message("创建人不能为空")
+	valid.Range(state, 0, 1, "state").Message("状态只允许0或1")
+
+	code := errorCode.INVALID_PARAMS
+	if !valid.HasErrors() {
+		ok, _ := models.ExistTagByID(tagId)
+		if  ok{
+			data := make(map[string]interface{})
+			data["tag_id"] = tagId
+			data["title"] = title
+			data["desc"] = desc
+			data["content"] = content
+			data["created_by"] = createdBy
+			data["state"] = state
+
+			models.AddArticle(data)
+			code = errorCode.SUCCESS
+		} else {
+			code = errorCode.ERROR_NOT_EXIST_TAG
+		}
+	} else {
+		for _, err := range valid.Errors {
+			log.Println(err.Key, err.Message)
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg": errorCode.GetMsg(code),
+		"data": make(map[string]interface{}),
+	})
 }
 
 // EditArticle 修改文章
